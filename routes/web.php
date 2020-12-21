ed29af4265c5ea10866870a8cad8d9106405b5fb
moshiur rahman <moshiur>
2020-12-21 12:41:35 +0600

    addmin template assets file support

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+// Admin Routes
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('/', 'Backend\DashboardController@index')->name('admin.dashboard');
+});
+
